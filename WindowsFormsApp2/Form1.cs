ebce27204a1d5092199c7604783fad7c9180c5e5
Jtuzi <944313281@qq.com>
2018-08-11 10:28:05 +0800

    TextComparison

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string Cmp( string m,string c)
+        {
+            m = m.Replace(",","|");
+            string res = "";
+            foreach (var item in m)
+            {
+                if(!c.Contains(item))
+                {
+                    res=res+item;
+                }
+            }
+            return res.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show(Cmp("GOD99,Win.,2845130560,dalele,asd4527,1483261260`,", "GOD99,Win.,"));
+        }
+    }
+}
